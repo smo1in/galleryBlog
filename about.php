7b03520a1d7e799337c4edd926543bbae30fcef9
smo1in <smolingm@gmail.com>
2020-08-10 16:00:47 +0700

    dividing layouts to header/footer

---
+++ about.php
@@ -0,0 +1,80 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+
+<!-- About Page -->
+<section class="about__page">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="about__text">
+                    <h3 class="about__title">About Me</h3>
+                    <div class="about__meta">
+                        <img src="img/profile.jpg" alt="">
+                        <div class="about__meta__info">
+                            <h5>Caleb Rodriguez</h5>
+                            <p>PHOTOGRAPHER / DESIGNER</p>
+                        </div>
+                    </div>
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Vivamus at nibh
+                        tincidunt, bibendum ligula id. Nemo enim ipsam voluptatem quiatotam rem aperiam, eaque ipsa quae
+                        ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                        ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat incididunt ut labore et dolore magna aliqua.</p>
+                    <img src="img/signature.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="experience__text">
+                    <h3 class="about__title">Experience</h3>
+                    <div class="experience__item">
+                        <h4>Envato Co.</h4>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Vivamus at nibh
+                            tincidunt, bibendum ligula id. Nemo enim ipsam voluptatem quiatotam rem aperiam, eaque ipsa
+                            quae ab illo inventore veritatis et quasi architecto beatae.</p>
+                    </div>
+                    <div class="experience__item">
+                        <h4>Unplash Co.</h4>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Vivamus at nibh
+                            tincidunt, bibendum ligula id. Nemo enim ipsam voluptatem quiatotam rem aperiam, eaque ipsa
+                            quae ab illo inventore veritatis et quasi architecto beatae.</p>
+                    </div>
+                    <div class="experience__item">
+                        <h4>Colorlib Inc.</h4>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Vivamus at nibh
+                            tincidunt, bibendum ligula id. Nemo enim ipsam voluptatem quiatotam rem aperiam, eaque ipsa
+                            quae ab illo inventore veritatis et quasi architecto beatae.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="skills__text">
+                    <h3 class="about__title">Skills</h3>
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Vivamus at nibh
+                        tincidunt, bibendum ligula id. </p>
+                    <div class="single-progress-item">
+                        <h6>Development</h6>
+                        <div class="progress-bar-style" data-progress="70"></div>
+                    </div>
+                    <div class="single-progress-item">
+                        <h6>APP Design</h6>
+                        <div class="progress-bar-style" data-progress="70"></div>
+                    </div>
+                    <div class="single-progress-item">
+                        <h6>Graphic Design</h6>
+                        <div class="progress-bar-style" data-progress="70"></div>
+                    </div>
+                    <div class="single-progress-item">
+                        <h6>Photography</h6>
+                        <div class="progress-bar-style" data-progress="70"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Page end -->
+
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
---
+++ views/layouts/footer.php
@@ -0,0 +1,31 @@
+<!-- Footer Section -->
+<footer class="footer__section">
+    <div class="container">
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        <div class="footer__copyright__text">
+            <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+        </div>
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    </div>
+</footer>
+<!-- Footer Section end -->
+
+<!-- Search Begin -->
+<div class="search-model">
+    <div class="h-100 d-flex align-items-center justify-content-center">
+        <div class="search-close-switch">+</div>
+        <form class="search-model-form">
+            <input type="text" id="search-input" placeholder="Search here.....">
+        </form>
+    </div>
+</div>
+<!-- Search End -->
+
+<!--====== Javascripts & Jquery ======-->
+<script src="js/vendor/jquery-3.2.1.min.js"></script>
+<script src="js/jquery.slicknav.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
